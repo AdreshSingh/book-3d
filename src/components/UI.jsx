798f4f1367f760d598ca8c49edd17b5ee7c1c688
AdreshSingh <adreshsingh271527@gmail.com>
2025-01-01 07:04:59 +0530

    UI part for book

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,4 +1,5 @@
 import { atom, useAtom } from "jotai";
+import { useEffect } from "react";
 
 const pictures = [
   "DSC00680",
@@ -41,36 +42,39 @@ pages.push({
 export const UI = () => {
   const [page, setPage] = useAtom(pageAtom);
 
+  useEffect(() => {
+    const audio = new Audio("/audios/page-flip-01a.mp3")
+    audio.play()
+  })
+
   return (
     <>
-      <main className=" pointer-events-none select-none z-10 fixed  inset-0  flex justify-between flex-col">
+      <main className="fixed inset-0 z-10 flex flex-col justify-between pointer-events-none select-none">
         <a
-          className="pointer-events-auto mt-10 ml-10"
+          className="mt-10 ml-10 pointer-events-auto"
           href="https://lessons.wawasensei.dev/courses/react-three-fiber"
         >
           <img className="w-20" src="/images/wawasensei-white.png" />
         </a>
-        <div className="w-full overflow-auto pointer-events-auto flex justify-center">
-          <div className="overflow-auto flex items-center gap-4 max-w-full p-10">
+        <div className="flex justify-center w-full overflow-auto pointer-events-auto">
+          <div className="flex items-center max-w-full gap-4 p-10 overflow-auto">
             {[...pages].map((_, index) => (
               <button
                 key={index}
-                className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                  index === page
-                    ? "bg-white/90 text-black"
-                    : "bg-black/30 text-white"
-                }`}
+                className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${index === page
+                  ? "bg-white/90 text-black"
+                  : "bg-black/30 text-white"
+                  }`}
                 onClick={() => setPage(index)}
               >
                 {index === 0 ? "Cover" : `Page ${index}`}
               </button>
             ))}
             <button
-              className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                page === pages.length
-                  ? "bg-white/90 text-black"
-                  : "bg-black/30 text-white"
-              }`}
+              className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${page === pages.length
+                ? "bg-white/90 text-black"
+                : "bg-black/30 text-white"
+                }`}
               onClick={() => setPage(pages.length)}
             >
               Back Cover
@@ -79,57 +83,57 @@ export const UI = () => {
         </div>
       </main>
 
-      <div className="fixed inset-0 flex items-center -rotate-2 select-none">
+      <div className="fixed inset-0 flex items-center select-none -rotate-2">
         <div className="relative">
-          <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
-            <h1 className="shrink-0 text-white text-10xl font-black ">
+          <div className="flex items-center gap-8 px-8 bg-white/0 animate-horizontal-scroll w-max">
+            <h1 className="font-black text-white shrink-0 text-10xl ">
               Wawa Sensei
             </h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">
+            <h2 className="italic font-light text-white shrink-0 text-8xl">
               React Three Fiber
             </h2>
-            <h2 className="shrink-0 text-white text-12xl font-bold">
+            <h2 className="font-bold text-white shrink-0 text-12xl">
               Three.js
             </h2>
-            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
+            <h2 className="italic font-bold text-transparent shrink-0 text-12xl outline-text">
               Ultimate Guide
             </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">
+            <h2 className="font-medium text-white shrink-0 text-9xl">
               Tutorials
             </h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
+            <h2 className="italic text-white shrink-0 text-9xl font-extralight">
               Learn
             </h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">
+            <h2 className="font-bold text-white shrink-0 text-13xl">
               Practice
             </h2>
-            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
+            <h2 className="italic font-bold text-transparent shrink-0 text-13xl outline-text">
               Creative
             </h2>
           </div>
-          <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
-            <h1 className="shrink-0 text-white text-10xl font-black ">
+          <div className="absolute top-0 left-0 flex items-center gap-8 px-8 bg-white/0 animate-horizontal-scroll-2 w-max">
+            <h1 className="font-black text-white shrink-0 text-10xl ">
               Wawa Sensei
             </h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">
+            <h2 className="italic font-light text-white shrink-0 text-8xl">
               React Three Fiber
             </h2>
-            <h2 className="shrink-0 text-white text-12xl font-bold">
+            <h2 className="font-bold text-white shrink-0 text-12xl">
               Three.js
             </h2>
-            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
+            <h2 className="italic font-bold text-transparent shrink-0 text-12xl outline-text">
               Ultimate Guide
             </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">
+            <h2 className="font-medium text-white shrink-0 text-9xl">
               Tutorials
             </h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
+            <h2 className="italic text-white shrink-0 text-9xl font-extralight">
               Learn
             </h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">
+            <h2 className="font-bold text-white shrink-0 text-13xl">
               Practice
             </h2>
-            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
+            <h2 className="italic font-bold text-transparent shrink-0 text-13xl outline-text">
               Creative
             </h2>
           </div>
