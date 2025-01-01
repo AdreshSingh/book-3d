70df52816d2b4c38f13771cbaaf91df41c3d84b7
AdreshSingh <adreshsingh271527@gmail.com>
2025-01-01 07:02:01 +0530

    modified entery point

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { Loader } from '@react-three/drei';
 
-import UI from './components/UI';
+
+import { UI } from './components/UI';
+import { Experience } from './components/Experience'
 
 function App() {
-  
+
 
   return (
     <>
-      <UI/>
+      <UI />
+      <Loader />
+      <Canvas shadows camera={{ position: [-0.5, 1, window.innerWidth > 800 ? 4 : 9], fov: 45 }} className=''>
+        <group position-y={0}>
+          <Suspense fallback={null} >
+            <Experience />
+          </Suspense>
+        </group>
+      </Canvas>
     </>
   )
 }
